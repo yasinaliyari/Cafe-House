b13c9d880d1295e60d9596048119e90ba3192d05
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-16 20:53:16 +0330

    create ContactUsForm

---
+++ home/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from home.models import ContactUs
+
+
+class ContactUsForm(forms.ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = ["name", "email", "phone", "subject", "message"]
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Your Name"}
+            ),
+            "email": forms.EmailInput(
+                attrs={"class": "form-control", "placeholder": "Your Email"}
+            ),
+            "subject": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Subject"}
+            ),
+            "message": forms.Textarea(
+                attrs={"class": "form-control", "rows": 5, "placeholder": "Message"}
+            ),
+        }
