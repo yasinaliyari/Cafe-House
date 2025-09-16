e5f05f984383ac216fe523477b8e55b19031c692
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-17 01:01:17 +0330

    create Slider model

--- home/models.py
+++ home/models.py
@@ -15,3 +15,20 @@ class ContactUs(models.Model):
     class Meta:
         verbose_name = "Contact Us Form"
         verbose_name_plural = "Contact Us Forms"
+
+
+class Slider(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField(upload_to="sliders/")
+    link = models.URLField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Slider Form"
+        verbose_name_plural = "Slider Forms"
