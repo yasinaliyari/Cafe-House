474b2c8016a243aada8ff56417c827ebfe3a75a7
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-17 02:03:00 +0330

    create Special model

--- home/models.py
+++ home/models.py
@@ -20,7 +20,7 @@ class ContactUs(models.Model):
 class Slider(models.Model):
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=255, blank=True, null=True)
-    image = models.ImageField(upload_to="sliders/")
+    image = models.ImageField(upload_to="home/sliders/")
     link = models.URLField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
@@ -36,8 +36,8 @@ class Slider(models.Model):
 
 class PopularItem(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=255)
-    image = models.ImageField(upload_to="popular_item")
+    description = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ImageField(upload_to="home/popular_item/")
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -47,3 +47,24 @@ class PopularItem(models.Model):
     class Meta:
         verbose_name = "Popular Item Form"
         verbose_name_plural = "Popular Item Forms"
+
+
+class Special(models.Model):
+    POSITION_CHOICES = (
+        ("left", "Left"),
+        ("right", "Right"),
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ImageField(upload_to="home/special/")
+    position = models.CharField(max_length=10, choices=POSITION_CHOICES, default="left")
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Special Form"
+        verbose_name_plural = "Special Forms"
