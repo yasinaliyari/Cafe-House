c215ec504fde9ce2a4137bde4e044b37945e610a
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-16 20:38:44 +0330

    add is_read field to ContactUs model

--- home/models.py
+++ home/models.py
@@ -7,6 +7,7 @@ class ContactUs(models.Model):
     subject = models.CharField(max_length=255)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.email}: {self.subject}"
