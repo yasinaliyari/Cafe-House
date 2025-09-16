059811a01ec225173bbc5800c7f1c48aafe492b7
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-16 23:23:54 +0330

    contact path

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from home.views import HomePageView
+from home.views import HomePageView, ContactUsView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("contact/", ContactUsView.as_view(), name="contact"),
 ]
