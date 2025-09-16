ea1e665f488db5ef081f0210c08d54419e6e0a7f
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-17 02:31:34 +0330

    create MenuItem model

--- home/models.py
+++ home/models.py
@@ -68,3 +68,30 @@ class Special(models.Model):
     class Meta:
         verbose_name = "Special Form"
         verbose_name_plural = "Special Forms"
+
+
+class MenuItem(models.Model):
+    CATEGORY_CHOICES = (
+        ("cold_drink", "Cold Drink"),
+        ("hot_drink", "Hot Drink"),
+        ("coffee_based", "Coffee Based"),
+        ("food", "Food"),
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=255, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.CharField(
+        max_length=20, choices=CATEGORY_CHOICES, default="coffee_based"
+    )
+    image = models.ImageField(upload_to="home/menu_item/", null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.get_category_display()} - {self.price}"
+
+    class Meta:
+        verbose_name = "Menu Item"
+        verbose_name_plural = "Menu Items"
+        ordering = ("category", "title")
