74021a81819edf7e2848591d854981dcc4873df2
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-16 23:22:20 +0330

    create ContactUsView

--- home/views.py
+++ home/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views import View
+from home.forms import ContactUsForm
+
+
+class ContactUsView(View):
+    def get(self, request):
+        form = ContactUsForm()
+        return render(request, "home/contact.html", {"form": form})
+
+    def post(self, request):
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your message has been sent successfully!")
+            return redirect("contact")
+        return render(request, "home/contact.html", {"form": form})
 
 
 class HomePageView(View):
