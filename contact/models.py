194a4be0f5fe73f0ba4b53a48c92a1faeb7f7421
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-17 15:28:35 +0330

    move ContactUs model to contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.email}: {self.subject}"
+
+    class Meta:
+        verbose_name = "Contact Us Form"
+        verbose_name_plural = "Contact Us Forms"
--- home/models.py
+++ home/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 
 
-class ContactUs(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    subject = models.CharField(max_length=255)
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.email}: {self.subject}"
-
-    class Meta:
-        verbose_name = "Contact Us Form"
-        verbose_name_plural = "Contact Us Forms"
-
-
 class Slider(models.Model):
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=255, blank=True, null=True)
