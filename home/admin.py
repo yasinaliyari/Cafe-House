b99d3101862cd238ae6b1da45b0d0c82d6884633
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-17 15:29:18 +0330

    register ContactUs

--- home/admin.py
+++ home/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from home.models import ContactUs, Slider, PopularItem, Special
-
-
-@admin.register(ContactUs)
-class ContactUsAdmin(admin.ModelAdmin):
-    list_display = ["name", "email", "subject", "is_read", "created_at"]
-    list_filter = ["is_read", "created_at"]
-    search_fields = ["name", "email", "subject", "message"]
+from home.models import Slider, PopularItem, Special
 
 
 @admin.register(Slider)
