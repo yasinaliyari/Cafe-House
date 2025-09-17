bd8b12d500817db9befef491b27e56074907e3e8
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-18 03:10:28 +0330

    create register, login, reset password views

--- account/views.py
+++ account/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from account.forms import RegisterForm
 
-# Create your views here.
+
+class RegisterView(FormView):
+    template_name = "account/register.html"
+    form_class = RegisterForm
+    success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
+
+
+class CustomLoginView(LoginView):
+    template_name = "account/login.html"
+
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy("home")
+
+
+class CustomPasswordResetView(PasswordResetView):
+    template_name = "account/password_reset_form.html"
+    email_template_name = "account/password_reset_email.html"
+    success_url = reverse_lazy("password_reset_done")
+
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    template_name = "account/password_reset_done.html"
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = "account/password_reset_confirm.html"
+    success_url = reverse_lazy("password_reset_complete")
+
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    template_name = "account/password_reset_complete.html"
