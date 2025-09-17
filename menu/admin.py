17540a08a73f8fc59e349da68702db8f9a90c400
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-17 03:32:25 +0330

    register MenuItem in menu app

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from menu.models import MenuItem
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "price", "is_active", "created_at")
+    list_filter = ("category", "is_active")
+    search_fields = ("title", "description")
