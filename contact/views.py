75044d8ff849d51c9c5f698f34ba0ff936d8eb76
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-17 15:47:02 +0330

    add ContactUsView to contact app

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views import View
+from contact.forms import ContactUsForm
 
-# Create your views here.
+
+class ContactUsView(View):
+    def get(self, request):
+        form = ContactUsForm()
+        return render(request, "contact/contact.html", {"form": form})
+
+    def post(self, request):
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your message has been sent successfully!")
+            return redirect("contact")
+        return render(
+            request,
+            "contact/contact.html",
+            {"form": form},
+        )
