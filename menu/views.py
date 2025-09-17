ccfd38248a0f5f48e78dc29cdbbec33bb481e8f4
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-17 15:55:00 +0330

    add MenuPageView

--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 from menu.models import MenuItem
 
 
-def menu_page(request):
-    items = MenuItem.objects.filter(is_active=True)
-    return render(request, "menu/menu.html", {"menu_items": items})
+class MenuPageView(TemplateView):
+    template_name = "menu/menu.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["cold_drinks"] = MenuItem.objects.filter(
+            category="cold_drink", is_active=True
+        )
+        context["hot_drinks"] = MenuItem.objects.filter(
+            category="hot_drink", is_active=True
+        )
+        context["coffee_based"] = MenuItem.objects.filter(
+            category="coffee_based", is_active=True
+        )
+        context["foods"] = MenuItem.objects.filter(category="food", is_active=True)
+
+        return context
