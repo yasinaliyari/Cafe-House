cf7fc380e0a0501acdff8fd3462046a0cfe70934
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-18 19:45:39 +0330

    register PasswordResetCode

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from account.models import PasswordResetCode
 
-# Register your models here.
+
+@admin.register(PasswordResetCode)
+class PasswordResetCodeAdmin(admin.ModelAdmin):
+    list_display = ("user", "code", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__username", "code")
