04aa583fe273d83777c58d5cf5ea24d99a5a7f88
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-18 04:19:25 +0330

    create PasswordResetCode model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
+import random
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class PasswordResetCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=4)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = str(random.randint(1000, 9999))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.code}"
